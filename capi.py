ea71e5b398f87b95e62403e1c59ae9b8e253815d
cleo-fe <mallola200.26@hotmail.com>
2016-07-13 02:29:59 -0500

    Aumente comentarios de que hace el programa, una bienvenida y mas formalidad al momento de pedir datos y dar el resultado

--- capi.py
+++ capi.py
@@ -1,11 +1,28 @@
-n = int(input("Ingrese un numero: "))
+#Programa que dira si un numero es capicua o no
+#Proyecto GitHub para windows
+
+print("Bienvenido")
+print()
+print("Este programa dira si el numero ingresado es capicua o no")
+print()
+
+n = int(input("Por favor ingrese un numero: "))
 s=0
 k=n;
+
 while(n!=0):
     r=n%10;
     s=s*10+r;
     n=n/10;
+    
 if(s==k):
-    print("Es capicua ");
+    print("El numero ingresado es capicua ");
+    
 else:
-    print("No es capicua ")
+    print("El numero ingresado no es capicua ")
+
+print()
+print("Hasta Luego")
+print()
+
+#Fin del programa
